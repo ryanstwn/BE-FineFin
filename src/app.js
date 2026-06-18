8337369db0ea4c6caeeb321cafdbe54f647f01ad
Ahmad-Mulyana <ahmadsuhendarto@gmail.com>
2026-06-18 15:36:32 +0700

    benerin cors di BE1/sprint5

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
-import express from "express"
-import morgan from "morgan"
-import helmet from "helmet"
-import cors from "cors"
-import mainRoute from "./routes/index.js"
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import mainRoute from "./routes/index.js";
 //import errorMiddleware from "./middlewares/errorMiddleware.js"
 
 const createApp = () => {
-    const app = express()
+  const app = express();
 
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(morgan("dev"))
-    app.use(helmet())
-    app.use(cors())
+  app.use(cors());
 
-    app.use('/api', mainRoute)
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+    }),
+  );
 
-    //app.use(errorMiddleware)
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan("dev"));
 
-    return app
-}
+  app.use("/api", mainRoute);
 
-export default createApp
+  //app.use(errorMiddleware)
+
+  return app;
+};
+
+export default createApp;
