c3639c1c3b6e3775b5544003febd784831d739c2
ryan <ryanstwn2317@gmail.com>
2026-07-18 14:23:46 +0700

    Menambahkan Robo Advisor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
 import authRouter from './authRoute.js'
+import financialRoute from './financialRoute.js'
+import transactionRoute from './transactionRoute.js'
+import roboAdvisorRoute from './roboAdvisorRoute.js'
 
 //Import rute onbordingController
 import financialRoute from './financialRoute.js'
@@ -11,5 +14,7 @@ mainRoute.use('/auth', authRouter)
 mainRoute.use('/onboarding', financialRoute)
 
 mainRoute.use('/transaction', transactionRoute)
+
+mainRoute.use{'/robo-advisor', roboAdvisorRoute}
 
 export default mainRoute
